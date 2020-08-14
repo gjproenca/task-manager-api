4d92c8fbadd792992a77e1f5d4643420c7f7b092
Goncalo Proenca <gjproenca@gmail.com>
2020-08-14 09:54:09 +0100

    add mocks, add more tests

---
+++ src/tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,5 @@
+// Created mocks for sendgrid so that emails are not sent each time tests run
+module.exports = {
+  setApiKey() {},
+  send() {},
+};
--- src/tests/user.test.js
+++ src/tests/user.test.js
@@ -1,12 +1,20 @@
 const request = require("supertest");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
 const app = require("../app");
 const User = require("../models/user");
-const { TestScheduler } = require("jest");
 
+const userOneId = new mongoose.Types.ObjectId();
 const userOne = {
+  _id: userOneId,
   name: "Mike",
   email: "mike@example.com",
   password: "123!!!tree123",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
 };
 
 beforeEach(async () => {
@@ -15,7 +23,7 @@ beforeEach(async () => {
 });
 
 test("Should sign up a new user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users")
     .send({
       name: "Goncalo",
@@ -23,14 +31,69 @@ test("Should sign up a new user", async () => {
       password: "fff12312",
     })
     .expect(201);
+
+  // Assert that the database was changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assertions about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: "Goncalo",
+      email: "goncalo@example.com",
+    },
+    token: user.tokens[0].token,
+  });
+  expect(user.password).not.toBe("fff12312");
 });
 
 test("Should login existing user", async () => {
-  await request(app)
+  const response = await request(app)
     .post("/users/login")
     .send({
       email: userOne.email,
       password: userOne.password,
     })
     .expect(200);
+
+  // Assert that token in response matches user second token
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token);
+});
+
+test("Should not login nonexistent user", async () => {
+  await request(app)
+    .post("/users/login")
+    .send({
+      email: "mikeadwdw@example.com",
+      password: "qwdqwree123",
+    })
+    .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await request(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+
+test("Sould not get profile for unauthenthicated user", async () => {
+  await request(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete account for user", async () => {
+  await request(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+
+  // Assert user has been deleted in database
+  expect(await User.findById(userOneId)).toBeNull();
+});
+
+test("Should not delete account for unauthenticathed user", async () => {
+  await request(app).delete("/users/me").send().expect(401);
 });
